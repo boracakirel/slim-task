73d26a44ef4b0fe0385781b692ef753c92e2e6c3
boracakirel <boracakirel@gmail.com>
2024-10-01 18:54:08 +0300

    Added database connection.

--- public/index.php
+++ public/index.php
@@ -1,12 +1,35 @@
 <?php
 
+use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container();
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
+
+$container->set('db', function () {
+    $settings = [
+        'host'     => '127.0.0.1',
+        'dbname'   => 'slim-task',
+        'username' => 'root',
+        'password' => 'root',
+    ];
+
+    $pdo = new PDO(
+        'mysql:host=' . $settings['host'] . ';dbname=' . $settings['dbname'],
+        $settings['username'],
+        $settings['password']
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    return $pdo;
+});
 
 $app->get('/', function (
     Request $request,
@@ -17,5 +40,17 @@ $app->get('/', function (
 
     return $response;
 });
+
+$app->get('/posts', function ($request, $response, $args) {
+    $pdo = $this->get('db');
+
+    $stmt  = $pdo->query('SELECT * FROM posts');
+    $users = $stmt->fetchAll();
+
+    $response->getBody()->write(json_encode($users, JSON_THROW_ON_ERROR));
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
 
 $app->run();
